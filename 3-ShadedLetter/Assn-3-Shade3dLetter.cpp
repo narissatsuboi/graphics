db83f9e3689d1438bd2d07d4b3ad27e99c569681
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-10-09 16:48:13 -0700

    shaded L starter code

--- 3-ShadedLetter/Assn-3-Shade3dLetter.cpp
+++ 3-ShadedLetter/Assn-3-Shade3dLetter.cpp
@@ -9,31 +9,16 @@ GLuint vBuffer = 0; // GPU buffer ID
 GLuint program = 0; // GLSL shader program ID
 vec2 mouseWas, mouseNow; // rotation control 
 
+// the letter "L"
+vec3 colors[] = {{0.8, 0.2, 0.7}, {0.5, 0.9, 0.1}, {0.2, 0.6, 0.9}, {0.7, 0.3, 0.6}, {0.9, 0.7, 0.2}, {0.4, 0.1, 0.8}};
 
-// a triangle (3 2D locations, 3 RGB colors)
-// vec2 points[] = { {-.9f, -.6f}, {0.f, .6f}, {.9f, -.6f} };
-// vec3 colors[] = { {0, 0, 1}, {1, 0, 0}, {0, 1, 0} };
 
-// the letter "B"
-vec3 colors[] = {
-	{0.8, 0.2, 0.7},
-	{0.5, 0.9, 0.1},
-	{0.2, 0.6, 0.9},
-	{0.7, 0.3, 0.6},
-	{0.9, 0.7, 0.2},
-	{0.4, 0.1, 0.8},
-	{0.6, 0.4, 0.7},
-	{0.1, 0.8, 0.5},
-	{0.8, 0.6, 0.1},
-	{0.3, 0.7, 0.5}
-};
+vec2 points[] = { {0.0f, 0.0f}, {50.0f, 0.0f}, {50.0f, 20.0f}, {20.0f, 20.0f}, {0.0f, 80.0f}, {20.0f, 80.0f} };
 
-vec2 points[] = {{125, 225}, {50, 50}, {200, 50}, {240, 100}, {240, 175},
-{185, 225}, {217, 260}, {217, 315}, {175, 350}, {50, 350}};
 int nPoints = sizeof(points) / sizeof(vec2);
-int triangles[][3] = {{0,1,2}, {0,2,3}, {0,3,4}, {0,4,5}, {0,5,6}, {0,6,7}, {0,7,8}, {0,8,9}, {0,9,1}};
+int triangles[][3] = { {0,1,2}, {0,2,3}, {0,4,5}, { 0,3,5}};
+
 int nTriangles = sizeof(triangles) / (3 * sizeof(int));
-//gl_Position = vec4(point, 0, 1);
 
 const char *vertexShader = R"(
 	#version 130
@@ -60,14 +45,12 @@ void Display() {
 	// rotate
 	mat4 view = RotateY(mouseNow.x) * RotateX(mouseNow.y); // compound transform 
 	SetUniform(program, "view", view);
-	
 	// clear background
 	glClearColor(1, 1, 1, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
 	// run shader program, enable GPU vertex buffer
 	glUseProgram(program);
 	glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
-
 	// connect GPU point and color buffers to shader inputs
 	VertexAttribPointer(program, "point", 2, 0, (void *) 0);
 	VertexAttribPointer(program, "color", 3, 0, (void *) sizeof(points));
