00be89e0f9940ba2ea07b4e1e8810d8251ffd0b6
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-11-20 19:35:54 -0800

    completed aerial flight animations

--- 9-Aerial/9-Assn-Aerial-Flight.cpp
+++ 9-Aerial/9-Assn-Aerial-Flight.cpp
@@ -1,4 +1,4 @@
-// SmoothMesh: smooth, Phong-shaded .obj mesh
+// Aerial animation with plane and propeller on Bezier curve
 
 #include <glad.h>
 #include <GLFW/glfw3.h>
@@ -21,9 +21,9 @@ GLuint		 program = 0;
 // window, camera
 int          winWidth = 800, winHeight = 800;
 Camera		 camera(0, 0, winWidth, winHeight, vec3(0, 0, 0), vec3(0, 0, -4.5f), 30, 0.001f, 500);
-vec3		 grn(.1f, .6f, .1f), yel(1, 1, 0), blu(0, 0, 1);
+vec3		 charcoalGrey(34.0f / 255.0f, 34.0f / 255.0f, 34.0f / 255.0f), hotPink(255.0f / 255.0f, 105.0f / 255.0f, 180.0f / 255.0f), 
+			 grn(.1f, .6f, .1f), orange(255.0f / 255.0f, 165.0f / 255.0f, 0.0f), blu(0, 0, 1);
 
-// meshes
 struct Mesh {
 	vector<vec3> points, normals;  // from .obj file
 	vector<vec2> uvs;              // from .obj file
@@ -56,16 +56,68 @@ struct Mesh {
 };
 Mesh body, prop;  
 
+class Bezier {
+public:
+	vec3* pts = NULL;					 // pointer to four control points
+	Bezier(vec3* pts) : pts(pts) { }
+	vec3 Position(float t) {
+		// curve starts at t = 0, ends at t = 1
+		float t2 = t * t, t3 = t * t2;
+		return (-t3 + 3 * t2 - 3 * t + 1) * pts[0] + (3 * t3 - 6 * t2 + 3 * t) * pts[1] + (3 * t2 - 3 * t3) * pts[2] + t3 * pts[3];
+	}
+	void Draw(int res = 50, float curveWidth = 3.5f, float meshWidth = 2.5f) {
+		vec3 lineColor = charcoalGrey, meshColor = charcoalGrey, pointColor = grn;
+		// draw curve as res number of straight segments
+		for (int i = 0; i < res; i++)
+			Line(Position((float)i / res), Position((float)(i + 1) / res), curveWidth, lineColor);
+		// draw control mesh
+		for (int i = 0; i < 3; i++)
+			LineDash(pts[i], pts[i + 1], meshWidth, meshColor, meshColor);
+		// draw control points
+		for (int i = 0; i < 4; i++)
+			Disk(pts[i], 5 * curveWidth, pointColor);
+	}
+	vec3 Velocity(float t) {
+		float t2 = t * t;
+		// compute derivative of curve
+		return (-3 * t2 + 6 * t - 3) * pts[0] + (9 * t2 - 12 * t + 3) * pts[1] + (6 * t - 9 * t2) * pts[2] + 3 * t2 * pts[3];
+	}
+	mat4 Frame(float t) {
+		vec3 p = Position(t), v = normalize(Velocity(t)), n = normalize(cross(v, vec3(0, 1, 0)));
+		vec3 b = normalize(cross(n, v));
+		mat4 m = mat4(
+			vec4(n[0], b[0], -v[0], p[0]),
+			vec4(n[1], b[1], -v[1], p[1]),
+			vec4(n[2], b[2], -v[2], p[2]),
+			vec4(0, 0, 0, 1)
+		);
+		return m;
+	}
+};
+vec3 path[] = {
+	{2 / 3.f,0,2 / 3.f},     {1,0,1 / 3.f},     {1,.1f,-1 / 3.f},               // curve1: path[0]-path[3]
+	{2 / 3.f,.1f,-2 / 3.f},  {1 / 3.f,.1f,-1},  {-1 / 3.f,.4f,-1},              // curve2: path[3]-path[6]
+	{-2 / 3.f,.4f,-2 / 3.f}, {-1,.4f,-1 / 3.f}, {-1,0,1 / 3.f},                 // curve3: path[6]-path[9]
+	{-2 / 3.f,0,2 / 3.f},    {-1 / 3.f,0,1},    {1 / 3.f,0,1}, {2 / 3.f,0,2 / 3.f}  // curve4: path[9]-path[12]
+};
+const int nPath = sizeof(path) / sizeof(vec3) - 1;                         // skip redundant path[12]
+
+Bezier bezier[] = { Bezier(&path[0]), Bezier(&path[3]), Bezier(&path[6]), Bezier(&path[9]) };
+const int nBezier = sizeof(bezier) / sizeof(Bezier);
+
+time_t startTime = clock();                                      // app start time
+float duration = 3;                                              // time to fly path 
+
+
 // lighting
 
-vec3		lights[] = { {.5, 0, 1}, {1, 1, 0} };
+vec3		lights[] = { {.5, 0, 1}, {1, 1, 0}, {0.1, 0.75, 0} };
 const int	nLights = sizeof(lights)/sizeof(vec3);
 
 // interaction
 
 void	   *picked = NULL;
 Mover		mover;
-
 
 // shaders
 
@@ -124,8 +176,13 @@ void Display(GLFWwindow *w) {
 	SetUniform(program, "nLights", nLights);
 	SetUniform3v(program, "lights", nLights, (float *) xLights);
 	// render plane parts
-	body.Render(grn);
+	body.Render(hotPink);
 	prop.Render(blu);
+	// draw flight path
+	UseDrawShader(camera.fullview);
+	for (int i = 0; i < 4; i++) {
+		bezier[i].Draw();
+	}
 	glFlush();
 }
 
@@ -161,15 +218,21 @@ void MouseWheel(float spin) {
 	camera.Wheel(spin, Shift());
 }
 
-
-
-
 // Application
 
 void Resize(int width, int height) {
 	camera.Resize(width, height);
 }
 
+
+void Animate() {
+	float elapsed = (float)(clock() - startTime) / CLOCKS_PER_SEC, a = nBezier * elapsed / duration;
+	float b = fmod(a, nBezier), t = b - floor(b);
+	int i = (int)floor(b);
+	mat4 f = bezier[i].Frame(t);
+	body.toWorld = f * Scale(.35f) * RotateY(-90);
+	prop.toWorld = body.toWorld * Translate(-.6f, 0, 0) * RotateY(-90) * Scale(.25f) * RotateZ(1500 * elapsed);
+}
 
 int main(int argc, char **argv) {
 	// enable anti-alias, init app window and GL context
@@ -184,8 +247,10 @@ int main(int argc, char **argv) {
 	RegisterMouseButton(MouseButton);
 	RegisterMouseWheel(MouseWheel);
 	RegisterResize(Resize);
+
 	// event loop
 	while (!glfwWindowShouldClose(w)) {
+		Animate();
 		Display(w);
 		glfwPollEvents();
 		glfwSwapBuffers(w);
